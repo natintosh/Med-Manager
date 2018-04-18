76a5121a46cc0fb5d7264322475ab97b1c807459
natintosh <ogunye4nao@gmail.com>
2018-04-18 08:20:19 +0100

    Minor changes made

--- app/src/main/java/com/example/oluwatobiloba/medmanager/utils/Months.java
+++ app/src/main/java/com/example/oluwatobiloba/medmanager/utils/Months.java
@@ -1,7 +1,7 @@
 package com.example.oluwatobiloba.medmanager.utils;
 
 public enum Months {
-    SELECT(0, "Select a month"),
+    SELECT(0, "All Months"),
     JANUARY(1, "January"), FEBRUARY(2, "February"), MARCH(3, "March"), APRIL(4, "April"),
     MAY(5, "May"), JUNE(6, "June"), JULY(7, "July"), AUGUST(8, "August"), SEPTEMBER(9, "September"),
     OCTOBER(10, "October"), NOVEMBER(11, "November"), DECEMBER(12, "December");
